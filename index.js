33cf714b4e761056ecdb7da62199e825af91cd58
Robert Garcia <77760521+robertgarcia305@users.noreply.github.com>
2023-07-30 12:31:56 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+    let body = document.getElementById("body");
+    let p = document.createElement("p");
+    p.innerHTML = "hello there";
+    body.appendChild(p);
+}
